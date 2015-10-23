da5aad34400a8b2da8b7d3f39b3809f83ad8db09
orangemug <info@orangemug.co.uk>
2015-10-23 23:42:45 +0100

    Made code match tutorial

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send("Hello World");
+  res.send("Hello from container land!");
 });
 
 var server = app.listen(process.env.PORT, function () {
